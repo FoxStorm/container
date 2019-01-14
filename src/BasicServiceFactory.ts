faa9c9940133079363278b383c3eff6efc967c7e
Sebastian Virlan <sebastian.virlan@bbc.co.uk>
2019-01-14 18:30:22 +0000

    Delete unneeded files

--- src/BasicServiceFactory.ts
+++
@@ -1,5 +0,0 @@
-// import { ServiceFactory } from './ServiceFactory'
-
-// export class BasicServiceFactory implements ServiceFactory {
-//   constructor(readonly serviceType: any, readonly serviceSupports: any[]) {}
-// }
