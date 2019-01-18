b585353acd10b2b86c8e315e1e5e0514f61f2497
Sebastian Virlan <sebastian.virlan@bbc.co.uk>
2019-01-18 10:03:02 +0000

    Export ConfigPreference

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { Config } from './src/Config'
+export { Config, ConfigPreference } from './src/Config'
 export { Environment } from './src/Environment'
 export { ApplicationContainer } from './src/ApplicationContainer'
 export { Services } from './src/Services'
